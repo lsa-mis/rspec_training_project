bf3c1e1ab2ae5453ad1a0a221812d6451c272c47
Margarita Barvinok <brita@umich.edu>
2023-11-24 16:34:10 -0500

    add more to the Stemmer class

--- stemmer.rb
+++ stemmer.rb
@@ -1,5 +1,30 @@
 class Stemmer
-  def stem(word)
-    word.chomp("ing")
+  SUFFIXES = ["ing", "ed", "s"]
+  PREFIXES = ["in", "over", "under", "multi", "un"]
+
+  def ending(word)
+    SUFFIXES.each do |suffix|
+      if word.end_with?(suffix)
+        return word.chomp(suffix)
+      end
+    end
+    return word
   end
+
+  def starting(word)
+    PREFIXES.each do |prefix|
+      if word.start_with?(prefix)
+        word.slice! prefix
+        return word
+      end
+    end
+    return word
+  end
+
+  def stem(word)
+    new_word = ending(word)
+    new_word = starting(new_word)
+    return new_word
+  end
+
 end
