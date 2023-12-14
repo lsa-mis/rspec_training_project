163a81ac6f203dd13e730643e6f1bee5ed03c544
Jocelyn Anderson <jocelynm@umich.edu>
2023-12-14 11:01:30 -0500

    changed stemmer to word

--- word_spec.rb
+++ word_spec.rb
@@ -4,52 +4,52 @@ require_relative './word.rb'
 
 RSpec.describe Word, "#stem" do
 
-  let!(:stemmer) { Word.new }
+  let!(:word) { Word.new }
   context "when the given word is 'training'" do
     it "returns 'train'" do
-      result = stemmer.stem('training')
+      result = word.stem('training')
       expect(result).to eq('train')
     end
   end
 
   context "when the given word is 'started'" do
     it "returns 'start'" do
-      result = stemmer.stem('started')
+      result = word.stem('started')
       expect(result).to eq('start')
     end
   end
 
   context "when the given word is 'dresses'" do
     it "returns 'dress'" do
-      result = stemmer.stem('dresses')
+      result = word.stem('dresses')
       expect(result).to eq('dress')
     end
   end
 
   context "when the given word is 'uncapable'" do
     it "returns 'capable'" do
-      result = stemmer.stem('uncapable')
+      result = word.stem('uncapable')
       expect(result).to eq('capable')
     end
   end
 
   context "when the given word is 'underestimated'" do
     it "returns 'estimat'" do
-      result = stemmer.stem('underestimated')
+      result = word.stem('underestimated')
       expect(result).to eq('estimat')
     end
   end
 
   context "when the given word is 'semiconductor'" do
     it "returns 'conduct'" do
-      result = stemmer.stem('semiconductor')
+      result = word.stem('semiconductor')
       expect(result).to eq('conduct')
     end
   end
 
   context "when the given word is 'semi-structured'" do
     it "returns 'structur'" do
-      result = stemmer.stem('semi-structured')
+      result = word.stem('semi-structured')
       expect(result).to eq('structur')
     end
   end
