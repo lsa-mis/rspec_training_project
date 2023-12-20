99881af799c3c97d5f55d5db829b8d183d7990fa
Margarita Barvinok <brita@umich.edu>
2023-12-20 12:10:42 -0500

    add sentence specs

--- sentence_spec.rb
+++ sentence_spec.rb
@@ -1,12 +1,20 @@
 require 'rspec'
+require_relative './sentence.rb'
 require_relative './word.rb'
 
 RSpec.describe Sentence, "#stem" do
 
-  context "when the given word is 'training'" do
-    it "returns 'train'" do
-      result = Sentence.new('the guinea pigs are training to eat carrots').stem
-      expect(result).to eq('the guinea pig are train to eat carrot')
+  context "when the given sentence is 'the guinea pigs are training to eat carrots'" do
+    it "returns 'guinea pig train eat carrot'" do
+      result = Sentence.new('the guinea pigs are training to eat carrots').stems
+      expect(result).to eq('guinea pig train eat carrot')
+    end
+  end
+
+  context "when the given sentence is 'Polonius: What do you read, my lord? Hamlet: Words, words, words.'" do
+    it "returns 'Polonius read lord Hamlet word'" do
+      result = Sentence.new('Polonius: What do you read, my lord? Hamlet: Words, words, words.').stems
+      expect(result).to eq('poloniu read lord hamlet word')
     end
   end
 
